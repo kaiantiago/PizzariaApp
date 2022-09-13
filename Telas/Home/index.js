4c297704e08ab2927f7ec6ec2eca5f8de8f102eb
kaiantiago <kaiantiago@outlook.com>
2022-09-13 20:48:53 -0300

    Home coding started

--- Telas/Home/index.js
+++ Telas/Home/index.js
@@ -1,3 +1,11 @@
 import { react } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './styles';
+
+export default function Home({navigation}) {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>try to open any screen (test)</Text>
+        </View>
+    )
+}
--- Telas/Home/styles.js
+++ Telas/Home/styles.js
@@ -1,1 +1,16 @@
 import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },
+    titulo: {
+        fontSize: 30,
+        marginBottom: 50
+    }
+})
+
+export default styles;
