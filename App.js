f2e62bb5e43124615aa53782c85f10e586064226
kaiantiago <kaiantiago@outlook.com>
2022-09-13 20:40:21 -0300

    Switch navigator added

--- App.js
+++ App.js
@@ -3,22 +3,22 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Home from '.Telas/Home/index';
 import Admin from '.Telas/Admin/index';
 import Carrinho from '.Telas/Admin/index';
+import Cadastro_Categorias from '.Telas/Cadastro_Categoria/index'
 import Cadastro_Produtos from '.Telas/Cadastro_Produtos/index';
+
+
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    Home,
+    Admin,
+    Carrinho,
+    Cadastro_Categorias,
+    Cadastro_Produtos
+  })
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
