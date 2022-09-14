66d3be486dcd53bec071991e926646298377b6a8
ArthurAMichelettiS <68759070+ArthurAMichelettiS@users.noreply.github.com>
2022-09-14 19:30:45 -0300

    some cards

---
+++ componentes/card_categoria/index.js
@@ -0,0 +1,28 @@
+import {
+    Text,TouchableOpacity, View, Image
+} from 'react-native';
+
+import styles from './styles';
+import { Ionicons, Entypo } from '@expo/vector-icons';
+
+
+export default function CardCategoria({categoria, removerElemento, editar}) {
+   return (
+       <View style={styles.contato} >
+
+           <Text style={styles.listaNome}> {categoria.descricao}</Text>
+
+           <View style={styles.dadosBotoesAcao}>
+               <TouchableOpacity onPress={() => removerElemento(categoria.idC)}>
+                   <Ionicons name="md-remove-circle" size={32} color="red" />
+               </TouchableOpacity>
+
+               <TouchableOpacity onPress={() => editar(categoria.idC)}>
+                   <Entypo name="edit" size={32} color="black" />
+               </TouchableOpacity>
+
+           </View>
+       </View>
+   );
+
+};
