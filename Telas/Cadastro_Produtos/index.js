07acce2ee3166af617cc1d9c71ffe7065a31f5c1
kaiantiago <kaiantiago@outlook.com>
2022-09-17 15:57:57 -0300

    Dropdown

--- Telas/Cadastro_Produtos/index.js
+++ Telas/Cadastro_Produtos/index.js
@@ -1,9 +1,17 @@
 import { react } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useState, useEffect } from 'react';
 
-export default function Cadastro_Produtos({navigation}) {
-    return(
+export default function Cadastro_Produtos({ navigation }) {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        { label: 'Apple', value: 'apple' },
+        { label: 'Banana', value: 'banana' }
+    ])
+    return (
         <View style={styles.container}>
             <View style={styles.areaBtnVoltar}>
                 <TouchableOpacity style={styles.btnVoltar} onPress={
@@ -12,6 +20,16 @@ export default function Cadastro_Produtos({navigation}) {
                     <Text style={styles.textBtnVoltar}> Voltar </Text>
                 </TouchableOpacity>
                 <Text style={styles.titulo}>Cadastro de Produtos</Text>
+            </View>
+            <View>
+                <Text style={styles.lblDropdown}>Selecione a categoria</Text>
+                <DropDownPicker open={open}
+                    value={value}
+                    items={items}
+                    setOpen={setOpen}
+                    setValue={setValue}
+                    setItems={setItems} style={styles.campoDrop}>
+                </DropDownPicker>
             </View>
         </View>
     )
